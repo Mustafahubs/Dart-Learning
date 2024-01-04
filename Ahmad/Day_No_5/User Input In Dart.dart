3b16d0ef2c5965f7d707a63946fff2b7022a368a
muhammadahmad78613 <612muhammadahmad@gmail.com>
2024-01-04 14:21:57 +0500

    new files

---
+++ Ahmad/Day_No_5/User Input In Dart.dart
@@ -0,0 +1,29 @@
+// we need to import input method in dart
+import 'dart:io';
+
+// string input in dart
+// void main() {
+//   print("Please enter your name:- ");
+//   String? name  = stdin.readLineSync();
+//   print("Hello ${name}, Asslam o Alikum!");
+// }
+
+// int input in dart
+
+// void main() {
+//   print("enter your age:- ");
+//   int? age = int.parse(stdin.readLineSync()!); 
+//   print('Your age is $age');
+// }
+
+// float intput
+void main() {
+  print("enter your age:- ");
+  double? age = double.parse(stdin.readLineSync()!); 
+  print('Your age is $age');
+}
+
+
+
+
+
--- jupyter_dart_kernel/src/jupyterdartkernel/__main__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from ipykernel.kernelapp import IPKernelApp
-from .kernel import dartkernel
-IPKernelApp.launch_instance(kernel_class=dartkernel)
-
